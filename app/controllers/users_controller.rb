95b60f5d2574ee7b78f63093ccd7b62e7f523123
Noah <crashlive112@gmail.com>
2022-03-23 17:26:48 -0400

    create user params and modify index to only display user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  
+
   def index
-    render json: User.all
+    render json: User.all, only: user_params, status: :ok
   end
 
+  private
+
+  def user_params
+    params.permit(:username, :email, :first_name, :last_name, :profession, :github)
+  end
 end
