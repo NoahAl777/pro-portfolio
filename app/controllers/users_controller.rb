1aa040ffeac17c9d5ef7cf80633ba525d12559bd
Noah <crashlive112@gmail.com>
2022-03-23 17:45:34 -0400

    add show to user controller and render user filtered by id params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
     render json: User.all, except: [:password, :created_at, :updated_at], status: :ok
   end
 
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user, except: [:password, :created_at, :updated_at], status: :ok
+    else
+      render json: { error: "user not found" }, status: :not_found
+    end
+  end
+
   private
 
   def user_params
