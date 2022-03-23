8f7ee5c836fa67ab25166830b63045de1c527bab
Noah <crashlive112@gmail.com>
2022-03-23 16:26:47 -0400

    Use faker library to create sample seed data for users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  User.create(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    profession: Faker::Job.title,
+    github: Faker::Internet.url(host: 'github.com')
+    )
+end
