6a32af251de7e43964e6961957e08e998e1f0b07
Noah <crashlive112@gmail.com>
2022-03-24 00:15:26 -0400

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_200444) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_034930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "technologies"
+    t.string "github"
+    t.string "live_link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -26,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_200444) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "projects", "users"
 end
