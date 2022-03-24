a1a4dcc222364efadad6aabaa6999e51ff137bb6
Noah <crashlive112@gmail.com>
2022-03-24 00:12:33 -0400

    add seeding code for projects and comment out user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
-10.times do
-  User.create(
-    username: Faker::Internet.username,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    firstname: Faker::Name.first_name,
-    lastname: Faker::Name.last_name,
-    profession: Faker::Job.title,
-    github: Faker::Internet.url(host: 'github.com')
+# 10.times do
+#   User.create(
+#     username: Faker::Internet.username,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,
+#     firstname: Faker::Name.first_name,
+#     lastname: Faker::Name.last_name,
+#     profession: Faker::Job.title,
+#     github: Faker::Internet.url(host: 'github.com')
+#     )
+# end
+
+25.times do
+  Project.create(
+    title: Faker::App.unique.name,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    technologies: "#{Faker::ProgrammingLanguage.name}, #{Faker::ProgrammingLanguage.name}, #{Faker::ProgrammingLanguage.name}",
+    github: Faker::Internet.url(host: 'github.com'),
+    live_link: Faker::Internet.url(host: 'herokuapp.com')
     )
 end
