f534f7c3034d52149074492525fc4b3bd8b59f1e
Noah <crashlive112@gmail.com>
2022-03-24 22:56:32 -0400

    add show to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,16 @@ class ProjectsController < ApplicationController
     render json: projects, include: :user
   end
 
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      project = user.projects.find(params[:id])
+    else
+      project = Project.find(params[:id])
+    end
+    render json: project, include: :user
+  end
+
 private
 
 def find_project
