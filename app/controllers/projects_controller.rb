ac1433767b4e4a20353b84a72d6ad1f45eae9818
Noah <crashlive112@gmail.com>
2022-03-24 23:24:10 -0400

    add create to projects controller and add private method project_params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,19 @@ class ProjectsController < ApplicationController
     render json: project, include: :user
   end
 
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      project = user.projects.create(project_params)
+    end
+    render json: project, include: :user
+  end
+  
 private
+
+def project_params
+  params.permit(:title, :description, :technologies, :github, :live_link)
+end
 
 def find_project
   Project.find(params[:id])
