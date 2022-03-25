a393b35e562a24cd6fc655b73da267cad7dc5262
Noah <crashlive112@gmail.com>
2022-03-24 22:47:54 -0400

    add index to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,19 @@
 class ProjectsController < ApplicationController
 
+  def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      projects = user.projects
+    else
+    projects = Project.all
+    end
+    render json: projects, include: :user
+  end
+
 private
 
 def find_project
-  project = Project.find(params[:id])
+  Project.find(params[:id])
 end
 
 end
