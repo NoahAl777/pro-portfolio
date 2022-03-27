97c69eb22b20fc18bfc13f0690884a4a170f9d22
Noah <crashlive112@gmail.com>
2022-03-27 00:19:02 -0400

    limit categories routes and add categories nested routes in projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :categories
-
+  
   resources :users, only: [:index, :show, :create, :update, :destroy] do
     resources :projects, only: [:index, :show, :create, :update, :destroy]
   end
+  
+  resources :projects, only: [:index, :show, :update, :destroy] do
+    resources :categories, only: [:index, :show]
+  end
 
-  resources :projects, only: [:index, :show, :update, :destroy]
+  resources :categories, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
