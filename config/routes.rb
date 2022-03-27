ca0626f3b17468000f46cce3acc4bb1913d41c7c
Noah <crashlive112@gmail.com>
2022-03-26 21:17:54 -0400

    generate resources for Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
 
   resources :users, only: [:index, :show, :create, :update, :destroy] do
     resources :projects, only: [:index, :show, :create, :update, :destroy]
