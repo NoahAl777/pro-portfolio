76a87e2ebaf24416566c7b1030817ffbfdf2afed
Noah <crashlive112@gmail.com>
2022-03-26 23:56:15 -0400

    add has_many relationship to projects and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :projects, through: :project_categories
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :categories, through: :project_categories
 end
