6e2cddfd79dcdb9ae8f451d8debf6444cfa8ce74
Noah <crashlive112@gmail.com>
2022-03-26 21:23:12 -0400

    add seeding code for Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,20 @@
 #     )
 # end
 
-25.times do
-  Project.create(
-    title: Faker::App.unique.name,
-    description: Faker::Lorem.paragraph(sentence_count: 2),
-    technologies: Faker::ProgrammingLanguage.name,
-    github: Faker::Internet.url(host: 'github.com'),
-    live_link: Faker::Internet.url(host: 'herokuapp.com'),
-    user_id: Faker::Number.between(from: 1, to: 10)
-    )
+# 25.times do
+#   Project.create(
+#     title: Faker::App.unique.name,
+#     description: Faker::Lorem.paragraph(sentence_count: 2),
+#     technologies: Faker::ProgrammingLanguage.name,
+#     github: Faker::Internet.url(host: 'github.com'),
+#     live_link: Faker::Internet.url(host: 'herokuapp.com'),
+#     user_id: Faker::Number.between(from: 1, to: 10)
+#     )
+# end
+
+15.times do
+  Category.create(
+    title: Faker::Company.industry,
+    description: Faker::Lorem.paragraph(sentence_count: 2)
+  )
 end
