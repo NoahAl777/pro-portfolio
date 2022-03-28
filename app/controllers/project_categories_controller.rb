72dc04965c9a22c3aeab21c5f3a0c1910eafb5a2
Noah <crashlive112@gmail.com>
2022-03-27 21:32:48 -0400

    add render not found response helper method

--- app/controllers/project_categories_controller.rb
+++ app/controllers/project_categories_controller.rb
@@ -1,3 +1,8 @@
 class ProjectCategoriesController < ApplicationController
+  
+  private
 
+  def render_not_found_response
+    render json: { error: "project category not found" }, status: :not_found
+  end
 end
