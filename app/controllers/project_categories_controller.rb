5d3b4573be067b58f3d35f818d46a8ab59ada9c7
Noah <crashlive112@gmail.com>
2022-03-27 21:43:12 -0400

    revert changes in project categories controller

--- app/controllers/project_categories_controller.rb
+++ app/controllers/project_categories_controller.rb
@@ -1,9 +1,3 @@
 class ProjectCategoriesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  
-  private
 
-  def render_not_found_response
-    render json: { error: "project category not found" }, status: :not_found
-  end
 end
