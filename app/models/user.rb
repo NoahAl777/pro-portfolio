226b0f6fd2d3cadd85cf6575e3e76e769f40557a
Noah <crashlive112@gmail.com>
2022-03-29 13:06:26 -0400

    add validation to email attr of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
