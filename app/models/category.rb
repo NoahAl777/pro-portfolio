230ba937945d119d9007802ca8071fdbdf77b902
Noah <crashlive112@gmail.com>
2022-03-31 12:02:03 -0400

    add validation to description attr of category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :project_categories
   has_many :projects, through: :project_categories
 
-  validates :title, presence: :true
-  validates :title, length: {in: 5..40}
+  validates :title, length: {in: 5..40}, allow_blank: false
+  validates :description, length: {in: 20..400}, allow_blank: false
 end
