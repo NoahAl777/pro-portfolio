4b4390d865aa34525744da4491e3f44446927a49
Noah <crashlive112@gmail.com>
2022-03-31 12:05:39 -0400

    refactor validations for better readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
   has_many :projects
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :password, presence: true
-  validates :password, length: { minimum: 8}
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8}
 end
