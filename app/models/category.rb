e0e6a36156037e2d1bf3fed26dbce970d0a51c55
Noah <crashlive112@gmail.com>
2022-03-31 11:45:22 -0400

    add validation to title attr of category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   has_many :project_categories
   has_many :projects, through: :project_categories
+
+  validates :title, presence: :true
+  validates :title, length: {in: 5..40}
 end
