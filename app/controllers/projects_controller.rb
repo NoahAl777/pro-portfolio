335e6fab9e6c6f69e1816502093da29c08da0a1f
Noah <crashlive112@gmail.com>
2022-03-31 12:34:09 -0400

    wrap parameters in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-
+  wrap_parameters format: []
+  
   def index
     if params[:user_id]
       user = find_user
