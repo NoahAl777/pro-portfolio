2d4081fb92e0f2ecac05d0dac9ed17a070a6064e
Noah <crashlive112@gmail.com>
2022-04-05 00:55:58 -0400

    test fetch

--- client/src/Components/ProfilesList.js
+++ client/src/Components/ProfilesList.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from "react";
 const ProfilesList = () => {
   const [profiles, setProfiles] = useState();
 
+  useEffect(() => {
+    fetch("/api/users")
+      .then(r => r.json())
+      .then(console.log)
+  })
+
   return (
     <div className="ProfilesList">
 
