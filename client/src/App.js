821bf9b696ff91d75a559a23919562b2be82b238
Noah <crashlive112@gmail.com>
2022-04-05 00:47:02 -0400

    import ProfilesList into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./Components/Home"
+import ProfilesList from './Components/ProfilesList';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <ProfilesList />
     </div>
   );
 }
