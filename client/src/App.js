f369935de4fe88b541d72b61ee15b877587c02e0
Noah <crashlive112@gmail.com>
2022-04-05 13:13:21 -0400

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home"
 import ProfilesList from './Components/ProfilesList';
 
@@ -17,8 +18,12 @@ function App() {
 
   return (
     <div className="App">
-      <Home />
-      <ProfilesList profiles={profiles} />
+      <Routes>
+        <Route path="/">
+          <Home />
+          <ProfilesList profiles={profiles} />
+        </Route>
+      </Routes>
     </div>
   );
 }
