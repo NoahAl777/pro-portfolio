d9d03a24e8259810840a94e1f22079dbaa773c20
Noah <crashlive112@gmail.com>
2022-04-05 10:22:40 -0400

    import useState and useEffect hooks into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 import Home from "./Components/Home"
 import ProfilesList from './Components/ProfilesList';
 
