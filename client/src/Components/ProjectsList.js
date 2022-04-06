d85fbc496abf1e7cc70ac6846cb8bc308899951e
Noah <crashlive112@gmail.com>
2022-04-06 13:00:05 -0400

    create ProjectsList component

---
+++ client/src/Components/ProjectsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectsList = () => {
+
+  return (
+    <div className="ProjectsList">
+      <h2>Projects</h2>
+    </div>
+  )
+};
+
+export default ProjectsList
