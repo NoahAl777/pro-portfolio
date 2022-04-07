35392387bc47d1faec36c12680edfe41b05b9c9c
Noah <crashlive112@gmail.com>
2022-04-06 21:49:02 -0400

    destructure selectedProfile and display data

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -3,11 +3,15 @@ import { useParams } from "react-router-dom";
 
 const Profile = ({ selectedProfile }) => {
   const params = useParams();
+  const { firstname, lastname, email, github, profession, username } = selectedProfile[0];
   console.log("selected profile", selectedProfile)
 
   return (
     <div className="Profile">
-      <h2>Profile</h2>
+      <h1>{firstname} {lastname} | {profession}</h1>
+      <h2>Contact me:</h2>
+      <p>Email: {email}</p>
+      <p>Github: {github}</p>
     </div>
   )
 };
