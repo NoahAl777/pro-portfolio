394617015eabfda2bcfdfc2087b6102554f7103d
Noah <crashlive112@gmail.com>
2022-04-06 22:13:50 -0400

    destructure currentProfile

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -4,14 +4,14 @@ import { useParams } from "react-router-dom";
 const Profile = ({ selectedProfile, setSelectedProfile }) => {
   const params = useParams();
   const [currentProfile, setCurrentProfile] = useState(selectedProfile[0])
-  // const { firstname, lastname, email, github, profession, username } = selectedProfile[0];
+  const { firstname, lastname, email, github, profession, username } = currentProfile;
   console.log(currentProfile)
   return (
     <div className="Profile">
-      {/* <h1>{firstname} {lastname} | {profession}</h1>
+      <h1>{firstname} {lastname} | {profession}</h1>
       <h2>Contact me:</h2>
       <p>Email: {email}</p>
-      <p>Github: {github}</p> */}
+      <p>Github: {github}</p>
     </div>
   )
 };
