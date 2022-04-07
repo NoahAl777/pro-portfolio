88e59e9b6e06cb8ed55315c56ee231dcab178433
Noah <crashlive112@gmail.com>
2022-04-06 20:58:16 -0400

    import useParams

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
-
+  const params = useParams();
+  console.log(params.profileId)
   return (
     <div className="Profile">
       <h2>Profile</h2>
