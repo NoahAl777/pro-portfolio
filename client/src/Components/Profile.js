3d069ed148e4344e2860b38d7422c97d513c0e71
Noah <crashlive112@gmail.com>
2022-04-06 22:12:56 -0400

    create currentProfile state and setSelectedProfile as default

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -3,15 +3,15 @@ import { useParams } from "react-router-dom";
 
 const Profile = ({ selectedProfile, setSelectedProfile }) => {
   const params = useParams();
-  const { firstname, lastname, email, github, profession, username } = selectedProfile[0];
-
-  console.log("current profile", currentProfile)
+  const [currentProfile, setCurrentProfile] = useState(selectedProfile[0])
+  // const { firstname, lastname, email, github, profession, username } = selectedProfile[0];
+  console.log(currentProfile)
   return (
     <div className="Profile">
-      <h1>{firstname} {lastname} | {profession}</h1>
+      {/* <h1>{firstname} {lastname} | {profession}</h1>
       <h2>Contact me:</h2>
       <p>Email: {email}</p>
-      <p>Github: {github}</p>
+      <p>Github: {github}</p> */}
     </div>
   )
 };
