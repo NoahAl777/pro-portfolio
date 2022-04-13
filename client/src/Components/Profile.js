4371b678f8fd06bfb8d3c801f586b650fef1b09c
Noah <crashlive112@gmail.com>
2022-04-13 14:10:34 -0400

    adjust useEffect fetch function

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -4,14 +4,25 @@ import { useParams } from "react-router-dom";
 const Profile = ({ selectedProfile, setSelectedProfile }) => {
   const params = useParams();
   const [currentProfile, setCurrentProfile] = useState(selectedProfile[0])
-  const { firstname, lastname, email, github, profession, username } = currentProfile;
+  // const { firstname, lastname, email, github, profession, username } = currentProfile;
 
+  useEffect(() => {
+    if (currentProfile == undefined) {
+      fetch(`/api/users/${params.profileId}`)
+        .then(r => r.json())
+        .then((data) => setCurrentProfile(data))
+    }
+  }, [])
+
+  const displayedProfile = currentProfile != undefined ? currentProfile : null
+  console.log("current profile", currentProfile)
+  console.log("displayed", displayedProfile)
   return (
     <div className="Profile">
-      <h1>{firstname} {lastname} | {profession}</h1>
+      <h1>{displayedProfile.firstname} {displayedProfile.lastname} | {displayedProfile.profession}</h1>
       <h2>Contact me:</h2>
-      <p>Email: {email}</p>
-      <p>Github: {github}</p>
+      <p>Email: {displayedProfile.email}</p>
+      <p>Github: {displayedProfile.github}</p>
     </div>
   )
 };
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-10.times do
-  User.create(
-    username: Faker::Internet.username,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    firstname: Faker::Name.first_name,
-    lastname: Faker::Name.last_name,
-    profession: Faker::Job.title,
-    github: Faker::Internet.url(host: 'github.com')
-    )
-end
+# 10.times do
+#   User.create(
+#     username: Faker::Internet.username,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,
+#     firstname: Faker::Name.first_name,
+#     lastname: Faker::Name.last_name,
+#     profession: Faker::Job.title,
+#     github: Faker::Internet.url(host: 'github.com')
+#     )
+# end
 
-25.times do
-  Project.create(
-    title: Faker::App.unique.name,
-    description: Faker::Lorem.paragraph(sentence_count: 2),
-    technologies: Faker::ProgrammingLanguage.name,
-    github: Faker::Internet.url(host: 'github.com'),
-    live_link: Faker::Internet.url(host: 'herokuapp.com'),
-    user_id: Faker::Number.between(from: 1, to: 10)
-    )
-end
+# 25.times do
+#   Project.create(
+#     title: Faker::App.unique.name,
+#     description: Faker::Lorem.paragraph(sentence_count: 2),
+#     technologies: Faker::ProgrammingLanguage.name,
+#     github: Faker::Internet.url(host: 'github.com'),
+#     live_link: Faker::Internet.url(host: 'herokuapp.com'),
+#     user_id: Faker::Number.between(from: 1, to: 10)
+#     )
+# end
 
-15.times do
-  Category.create(
-    title: Faker::Company.industry,
-    description: Faker::Lorem.paragraph(sentence_count: 2)
-  )
-end
+# 15.times do
+#   Category.create(
+#     title: Faker::Company.industry,
+#     description: Faker::Lorem.paragraph(sentence_count: 2)
+#   )
+# end
 
-50.times do
-  ProjectCategory.create(
-    project_id: Faker::Number.between(from: 3, to: 13),
-    category_id: Faker::Number.between(from: 1, to: 15)
-  )
-end
+# 50.times do
+#   ProjectCategory.create(
+#     project_id: Faker::Number.between(from: 3, to: 13),
+#     category_id: Faker::Number.between(from: 1, to: 15)
+#   )
+# end
