5f4bf7756d53c31b7391a8e1b5367aa08cd06db7
Noah <crashlive112@gmail.com>
2022-04-14 12:23:04 -0400

    import Link into Projects

--- client/src/Components/ProjectsList.js
+++ client/src/Components/ProjectsList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ProjectsList = ({ projects }) => {
   const [projectsToDisplay, setProjectsToDisplay] = useState([])
