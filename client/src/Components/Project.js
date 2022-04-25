0d136d5925a4c6b8c93ce7ad4e367a9aced888fe
Noah <crashlive112@gmail.com>
2022-04-25 16:04:15 -0400

    destructure currentProfile and display information inside Project div

--- client/src/Components/Project.js
+++ client/src/Components/Project.js
@@ -5,10 +5,12 @@ const Project = (selectedProject) => {
 
   console.log("current project", currentProject.selectedProject)
   // debugger
-
+  const { title, description, technologies, github } = currentProject.selectedProject
   return (
     <div className="Project">
-      <h2>Project</h2>
+      <h2>{`${title} - ${technologies}`}</h2>
+      <p>{`Description: ${description}`}</p>
+      <p>{`Github: ${github}`}</p>
     </div>
   )
 };
