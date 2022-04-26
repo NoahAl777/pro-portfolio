d9a540ad52c491eeb5aeb456d6d9734aae921e17
Noah <crashlive112@gmail.com>
2022-04-26 16:23:20 -0400

    useParams to fetch for instance if it is not accessed through the index menu

--- client/src/Components/Project.js
+++ client/src/Components/Project.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const Project = (selectedProject) => {
-  const [currentProject, setCurrentProject] = useState(selectedProject)
+const Project = ({ selectedProject }) => {
+  const params = useParams()
+  const [currentProject, setCurrentProject] = useState([selectedProject])
 
-  console.log("current project", currentProject.selectedProject)
+  // console.log("selected", selectedProject)
+  console.log("current", currentProject)
   // debugger
-  if (currentProject.selectedProject.length != 0) {
-    const { title, description, technologies, github } = currentProject.selectedProject
+
+  useEffect(() => {
+    if (Object.keys(currentProject[0]).length == 0) {
+      fetch(`/api/projects/${params.projectId}`)
+        .then(r => r.json())
+        .then(data => setCurrentProject([data]))
+        .then(console.log("fetched"))
+    } else {
+      setCurrentProject([selectedProject])
+    }
+  }, [])
+
+  // debugger
+  if (currentProject[0].length != 0) {
+    const { title, description, technologies, github } = currentProject[0]
     return (
       <div className="Project">
         <h2>{`${title} - ${technologies}`}</h2>
