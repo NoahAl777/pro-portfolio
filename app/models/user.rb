6e6836bc021e79b30f281462f78c27c1f9d4635a
Noah <crashlive112@gmail.com>
2022-05-03 15:01:03 -0400

    enforce referential integrity between users and projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :projects
-
+  has_many :projects, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8}
