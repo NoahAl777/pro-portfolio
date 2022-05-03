6118fd3cc9bc91962001b6a3f27e09fb1a63c41b
Noah <crashlive112@gmail.com>
2022-05-03 14:57:17 -0400

    eliminate unnecessary validation of presence in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8}
+  validates :password, length: { minimum: 8}
 end
