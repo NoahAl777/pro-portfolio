78a1a7dbebac66b482af21edea455afa65489aa2
Noah <crashlive112@gmail.com>
2022-05-16 19:57:16 -0400

    make login form a controlled form

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -4,17 +4,17 @@ const Login = () => {
   const [formData, setFormData] = useState({ username: "", password: "" })
 
   const handlChange = (event) => {
-    console.log(event.target.value)
+    setFormData({ ...formData, [event.target.id]: event.target.value })
   }
 
   return (
     <div className="Login">
       <form>
         <label>Username</label>
-        <input type="text" onChange={handlChange}></input>
+        <input type="text" id="username" onChange={handlChange}></input>
         <br />
         <label>Password</label>
-        <input type="text" onChange={handlChange}></input>
+        <input type="text" id="password" onChange={handlChange}></input>
         <br />
         <input type="submit" value="Login"></input>
       </form>
