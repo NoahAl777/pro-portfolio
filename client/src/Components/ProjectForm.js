eb45041fe8c70c8088059445b372fb639f320728
Noah <crashlive112@gmail.com>
2022-05-18 22:33:46 -0400

    make sure to wrap form in from tags

--- client/src/Components/ProjectForm.js
+++ client/src/Components/ProjectForm.js
@@ -3,25 +3,27 @@ import React from 'react';
 const ProjectForm = () => {
   // NOTE: When navigating to other pages from this component they don't load
   return (
-    <div className='ProjectForm'>
+    <>
       <h2>Create New Project</h2>
-      <label>Title</label>
-      <input type="text" id="title"></input>
-      <br />
-      <label>Technologies</label>
-      <input type="text" id="technologies"></input>
-      <br />
-      <label>Description</label>
-      <input type="text" id="description"></input>
-      <br />
-      <label>Github</label>
-      <input type="text" id="github"></input>
-      <br />
-      <label>Live Link</label>
-      <input type="text" id="live_link"></input>
-      <br />
-      <input type="submit" value="Add Project" />
-    </div>
+      <form className='ProjectForm'>
+        <label>Title</label>
+        <input type="text" id="title"></input>
+        <br />
+        <label>Technologies</label>
+        <input type="text" id="technologies"></input>
+        <br />
+        <label>Description</label>
+        <input type="text" id="description"></input>
+        <br />
+        <label>Github</label>
+        <input type="text" id="github"></input>
+        <br />
+        <label>Live Link</label>
+        <input type="text" id="live_link"></input>
+        <br />
+        <input type="submit" value="Add Project" />
+      </form>
+    </>
   )
 };
 
