87959d6435671c508d23fc25259df9b8c049ea79
Noah <crashlive112@gmail.com>
2022-05-18 22:50:38 -0400

    make form a controlled form

--- client/src/Components/ProjectForm.js
+++ client/src/Components/ProjectForm.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 // NOTE: When navigating to other pages from this component they don't load
-
+// NOTE: Until sessions problem is solved: all submissions will have user_id harcoded
 const ProjectForm = () => {
-  const [formData, setFormData] = useState({ title: '', description: '', technologies: '', github: '', live_link: '' })
+  const [formData, setFormData] = useState({ title: '', description: '', technologies: '', github: '', live_link: '', user_id: 20 })
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value })
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -13,19 +17,19 @@ const ProjectForm = () => {
       <h2>Create New Project</h2>
       <form className='ProjectForm' onSubmit={handleSubmit}>
         <label>Title</label>
-        <input type="text" id="title"></input>
+        <input type="text" id="title" onChange={handleChange}></input>
         <br />
         <label>Technologies</label>
-        <input type="text" id="technologies"></input>
+        <input type="text" id="technologies" onChange={handleChange}></input>
         <br />
         <label>Description</label>
-        <input type="text" id="description"></input>
+        <input type="text" id="description" onChange={handleChange}></input>
         <br />
         <label>Github</label>
-        <input type="text" id="github"></input>
+        <input type="text" id="github" onChange={handleChange}></input>
         <br />
         <label>Live Link</label>
-        <input type="text" id="live_link"></input>
+        <input type="text" id="live_link" onChange={handleChange}></input>
         <br />
         <input type="submit" value="Add Project" />
       </form>
