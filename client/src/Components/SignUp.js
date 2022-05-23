c85d9301d21511930e0866658ed7b4968d5c7b3f
Noah <crashlive112@gmail.com>
2022-05-23 10:27:47 -0400

    add handleChange function and correct typo

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -2,29 +2,33 @@ import React from "react";
 
 const SignUp = () => {
 
+  const handleChange = (event) => {
+    console.log(event)
+  }
+
   return (
     <div className="SignUp">
       <form onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" id="username" onChange={handlChange}></input>
+        <input type="text" id="username" onChange={handleChange}></input>
         <br />
         <label>Password</label>
         <input type="password" id="password" onChange={handleChange}></input>
         <br />
         <label>Email</label>
-        <input type="text" id="email" onChange={handlChange}></input>
+        <input type="text" id="email" onChange={handleChange}></input>
         <br />
         <label>First Name</label>
-        <input type="text" id="firstname" onChange={handlChange}></input>
+        <input type="text" id="firstname" onChange={handleChange}></input>
         <br />
         <label>Last Name</label>
-        <input type="text" id="lastname" onChange={handlChange}></input>
+        <input type="text" id="lastname" onChange={handleChange}></input>
         <br />
         <label>Profession</label>
-        <input type="text" id="profession" onChange={handlChange}></input>
+        <input type="text" id="profession" onChange={handleChange}></input>
         <br />
         <label>GitHub</label>
-        <input type="text" id="github" onChange={handlChange}></input>
+        <input type="text" id="github" onChange={handleChange}></input>
         <br />
         <input type="submit" value="Login"></input>
       </form>
