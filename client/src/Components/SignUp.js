7445b89e24c168e701a658c934f1c2f6f001d6c8
Noah <crashlive112@gmail.com>
2022-05-23 10:26:02 -0400

    add SignUp form

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -3,8 +3,31 @@ import React from "react";
 const SignUp = () => {
 
   return (
-    <div>
-      Sign Up Component
+    <div className="SignUp">
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="text" id="username" onChange={handlChange}></input>
+        <br />
+        <label>Password</label>
+        <input type="password" id="password" onChange={handleChange}></input>
+        <br />
+        <label>Email</label>
+        <input type="text" id="email" onChange={handlChange}></input>
+        <br />
+        <label>First Name</label>
+        <input type="text" id="firstname" onChange={handlChange}></input>
+        <br />
+        <label>Last Name</label>
+        <input type="text" id="lastname" onChange={handlChange}></input>
+        <br />
+        <label>Profession</label>
+        <input type="text" id="profession" onChange={handlChange}></input>
+        <br />
+        <label>GitHub</label>
+        <input type="text" id="github" onChange={handlChange}></input>
+        <br />
+        <input type="submit" value="Login"></input>
+      </form>
     </div>
   )
 };
